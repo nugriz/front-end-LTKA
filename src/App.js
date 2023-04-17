263e45b8556486af0463fc12a1f2214ca2048ee0
nugriz <rizki@happy5.co>
2023-04-17 16:36:06 +0700

    test connectivity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
 import React, { useState, useEffect } from 'react';
+export default function App() {
+  const [data, setData] = useState([])
+  const [respond, setRespond] = useState({})
+  const [formData, setFormData] = useState('')
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
+    fetchGames() // Fetch games when component is mounted
+  }, [])
+
+  const fetchGames = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((result) => setData(result))
+      .catch((err) => console.log('error'))
+  }
+
+  const saveGames = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: formData, // Use your own property name / key
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => setRespond(result))
+      .catch((err) => console.log('error'))
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    saveGames() // Save games when form is submitted
+  }
+
+  const handleChange = (event) => {
+    setFormData(event.target.value)
+  }
 
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        test: Click me
-      </button>
-    </div>
-  );
-}
+    <div className="App">
+      {/* method="post" not needed here because `fetch` is doing the POST not the `form` */}
+      {/* Also, note I changed the function name, handleSubmit */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" value={formData} onChange={handleChange} />
+        <button type="submit">click</button>
+      </form>
+      {respond['title']}
 
-export default App;
+      {data &&
+        data.map((element, index) => (
+          <div>{element.title} {element.body} </div>
+        ))}
+    </div>
+  )
+}
